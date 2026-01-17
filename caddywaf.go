10639cab881bc71910e8087b5768b50b6b938020
Fabrizio Salmi <fabrizio.salmi@gmail.com>
2026-01-18 00:15:33 +0100

    chore: update version constant to v0.2.0

--- caddywaf.go
+++ caddywaf.go
@@ -49,7 +49,7 @@ var (
 )
 
 // Add or update the version constant as needed
-const wafVersion = "v0.1.3" // update this value to the new release version when tagging
+const wafVersion = "v0.2.0" // update this value to the new release version when tagging
 
 // ==================== Initialization and Setup ====================
 
